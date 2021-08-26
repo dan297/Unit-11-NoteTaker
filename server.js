a02864a3f8d1cd8ab6735d144f952e641c100924
Dan <Danielwhite98@icloud.com>
2021-08-26 23:49:21 +0100

    Notes page button works

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const express = require("express");
 const { v1: uuidv1 } = require('uuid');
 const fs = require("fs");
 const path = require("path");
+const { readAndAppend, readFromFile } = require('./helpers/fsutils');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
+     })
+
+app.get('/api/notes', (req, res) => {
+    const notes = fs.readFile('db/db.json')
+    res.json(JSON.parse(notes))
 })
 
+app.post('/', (req, res) => {
+    const notes = fs.readFile('db/db.json')
+
+})
+
+// app.get('/notes', (req, res) => {
+// res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
+
+
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
